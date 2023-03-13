a87ef9c17bf2be6a3a5d9fb898b22da2c4b727ce
milosdjurica <milosdjurica13@gmail.com>
2023-03-13 23:39:45 +0100

    updated footer link

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -11,7 +11,7 @@ function Footer() {
         Created by{" "}
         <a
           className="hover:text-blue-500"
-          href="https://portfolio-milosdjurica.vercel.app/"
+          href="https://milosdjurica.vercel.app/"
         >
           Miloš Đurica
         </a>
